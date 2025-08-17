68e6190a432279fad92ae64b2e6ed63efcea10bf
oraichezakariyae <164167114+oraichezakariyae@users.noreply.github.com>
2025-08-17 13:51:23 +0100

    Update rush04.c

--- rush04.c
+++ rush04.c
@@ -1,35 +1,77 @@
-void ft_putchar(char c);
+void	ft_putchar(char c);
 
-void rush04(int x,int y){
+void	error_msg(char *str)
+{
+	int	i;
 
-    int i=0;
-    int j;
-    while(i<x){
-        j=0;
-        while(j<y){
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
 
-            if((i==0 && j==0) || ( i == x-1 && j== y-1 && x-1 > 0 && y-1 > 0 ))
-              ft_putchar('A');
+void	print_first_line(int x, int col_pos)
+{
+	while (col_pos < x)
+	{
+		if (col_pos == 0)
+			ft_putchar('A');
+		else if (col_pos == x - 1)
+			ft_putchar('C');
+		else
+			ft_putchar('B');
+		col_pos++;
+	}
+	ft_putchar('\n');
+}
 
+void	print_middle_line(int x, int col_pos)
+{
+	while (col_pos < x)
+        {
+		if (col_pos == 0 || col_pos == x - 1)
+			ft_putchar('B');
+		else
+			ft_putchar(' ');
+                col_pos++;
+        }
+	ft_putchar('\n');
+}
 
-            else if ((i == 0 &&   j == y - 1  ) ||  (i == x-1 &&  j == 0))
-            ft_putchar('C');
-            
-            else if(i==0|| i==x - 1)
-            ft_putchar('B');
+void	print_last_line(int x, int col_pos)
+{
+	while (col_pos < x)
+        {
+		if (col_pos == 0)
+			ft_putchar('C');
+		else if (col_pos == x - 1)
+			ft_putchar('A');
+		else
+			ft_putchar('B');
+                col_pos++;
+        }
+}
 
-            else if(j==0 || j==y - 1) 
-            ft_putchar('B');
-            
-            else
-            ft_putchar(' ');
+void	rush(int x, int y)
+{
+	int	row_pos;
+	int	col_pos;
 
-            j++;
-
-
-        } 
-       ft_putchar('\n');
-        i++;
-        
-    }  
+	if (x <= 0 || y <= 0)
+		error_msg("Invalid input!Try again.");
+	row_pos = 0;
+	col_pos = 0;
+	while (row_pos < y)
+	{
+		if (row_pos == 0)
+			print_first_line(x, col_pos);
+		else if (row_pos == y - 1)
+			print_last_line(x, col_pos);
+		else
+			print_middle_line(x, col_pos);
+		row_pos++;
+	}
 }
